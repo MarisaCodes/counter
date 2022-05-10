6a38c039e1c631731481c47777c98e5f1a60cb43
MarisaCodes <marisakirisame955@gmail.com>
2022-05-10 17:49:07 +0400

    first commit here test

---
+++ README.md
@@ -0,0 +1,6 @@
+# counter
+Increment by 1, decrement by 1
+
+This is a really basic repo for practice. It's practically a code dump from my practice.
+
+The counter: https://marisacodes.github.io/counter/
---
+++ counter.js
@@ -0,0 +1,11 @@
+let count = 0;
+increment = () => {
+    document.getElementById("btn").innerText = count+=2;
+};
+
+decrement = () => {
+  if (count>0) {
+      count--;
+  };
+  document.getElementById("btn").innerText = count;
+}
